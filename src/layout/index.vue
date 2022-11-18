85164d39b438ceea3e947b967aa954e2353ee73e
JacKlinYao <13248020250@163.com>
2022-11-18 17:41:42 +0800

    '环境搭建‘

---
+++ src/layout/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-container>
+      <el-aside width="200px">
+      <AdminMenu/>
+      </el-aside>
+      <el-container>
+        <el-header>
+          <AdminHeader/>
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    
+</template>
+
+<script setup>
+import AdminMenu from './components/AdminMenu.vue';
+import AdminHeader from './components/AdminHeader.vue';
+</script>
+
+<style lang="scss" scoped>
+.el-container {
+  height: 100vh;
+  // weight: 100vh;
+}
+.el-header {
+  background-color: #fff;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.el-aside {
+  // width: auto;
+  background-color: #ffefef;
+  color: #333;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
+</style>
